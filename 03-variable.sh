efa44790d55dc2bf9620831b383ba0690c4f9e2b
sivabommidi19-svg <sivabommidi19@gmail.com>
2025-09-24 22:59:10 +0530

    some message

---
+++ 03-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#No space before or after equal
+
+PERSON1=John
+PERSON2=Raheem
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: hellow $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: what are you learning recently?"
+echo "$PERSON2:: i am learning shell scripting $PERSON2"
