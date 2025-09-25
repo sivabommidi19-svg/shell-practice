0d6b33bbfa3916032dde1a7dbd41c14ad4c2f017
sivabommidi19-svg <sivabommidi19@gmail.com>
2025-09-25 22:41:28 +0530

    variables

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -2,3 +2,8 @@
 
 echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $*"
+echo "current directory:$PWD"
+echo "Script name: $0"
+echo "Who is running this: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of this script:$$"
