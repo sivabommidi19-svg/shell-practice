1ee94339a88608e77fa275a8861308cf7f09ef12
sivabommidi19-svg <sivabommidi19@gmail.com>
2025-09-25 22:07:52 +0530

    variables

--- 04-variable.sh
+++ 04-variable.sh
@@ -2,7 +2,7 @@
  
 PERSON1=$1
 PERSON2=$2
-echo "$PERSON1:: Welcome to alaska Putin"
+echo "$PERSON1:: Welcome to alaska $PERSON1"
 echo "$PERSON2:: Thanks you $PERSON1, Finally not on zoom webiner"
 echo "$PERSON1:: So, Do we hava deal"
 echo "$PERSON2:: Yes , if you make ukraine joins russia"
---
+++ 05-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "please enter your PIN number"
+read PIN_NUMBER #here PIN_NUMBER is Variable
+
+echo "Pin enterd is: $PIN_NUMBER"
