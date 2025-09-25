4a8b39fbaa82eecfe73116f70cc77f4823429e89
sivabommidi19-svg <sivabommidi19@gmail.com>
2025-09-25 21:42:34 +0530

    variables

---
+++ 04-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ 
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: Welcome to alaska Putin"
+echo "$PERSON2:: Thanks you $PERSON1, Finally not on zoom webiner"
+echo "$PERSON1:: So, Do we hava deal"
+echo "$PERSON2:: Yes , if you make ukraine joins russia"
