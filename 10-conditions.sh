032922d8d8c14c1f757b76ff0f4cc5eb303fe03e
sivabommidi19-svg <sivabommidi19@gmail.com>
2025-09-30 21:58:04 +0530

    session-06

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -4,6 +4,8 @@ NUMBER=$1
 
 if [ $NUMBER -lt 10 ]; then
     echo "Given number $NUMBER is Less then 10"
+elif[ $NUMBER -gq 10 ]; then
+    echo "Given Number $NUMBER is equal to 10"
 else
     echo "Given number $NUMBER is greater then or equal to 10"
 fi 
