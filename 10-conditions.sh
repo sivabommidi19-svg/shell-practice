08d2b3434a7bba210c2b635b623abe3171e5949f
sivabommidi19-svg <sivabommidi19@gmail.com>
2025-09-30 18:28:25 +0530

    session-o6

---
+++ 10-conditions.sh
@@ -0,0 +1,14 @@
+#!/bin/dash
+
+NUMBER=$1
+
+if [$NUMBER -lt 10]; then
+    echo "Given number $NUMBER is Less then 10"
+else
+    echo "Given number $NUMBER is granter then or equal to 10"
+fi 
+ 
+ # -gt
+ # -eq
+ # -ne
+ 
