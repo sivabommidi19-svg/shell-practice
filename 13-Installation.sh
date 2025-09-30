591df4369d8b98647f2bbe77549345f8930869f8
sivabommidi19-svg <sivabommidi19@gmail.com>
2025-09-30 23:23:36 +0530

    installation3

--- 13-Installation.sh
+++ 13-Installation.sh
@@ -6,6 +6,7 @@ if [ $USERID -ne 0 ]; then
     echo "Error:: please run this script with root privelege"
     exit 1 # failure is other than 0
 fi
+
 VALIDATE(){ # funsction receive the inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
         echo "Error:: Installing $2 is failure"
@@ -16,10 +17,10 @@ VALIDATE(){ # funsction receive the inputs through args just like shell script a
 }
 
 dnf install mysql -y
-VALIDATE $? "MYSQL"
+VALIDATE $? "MySQL"
 
 dnf install nginx -y
-VALIDATE $? "ngnix"
+VALIDATE $? "Ngnix"
 
 dnf install python3 -y
 VALIDATE $? "python3"
