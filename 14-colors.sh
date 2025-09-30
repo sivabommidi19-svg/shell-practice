5c296f99d80fc8d1c2cffc96b3e27979ea954303
sivabommidi19-svg <sivabommidi19@gmail.com>
2025-10-01 00:19:10 +0530

    colour

--- 14-colors.sh
+++ 14-colors.sh
@@ -5,5 +5,41 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-echo -e "$G Hello World  $N"
-echo "Check this colour"
+if [ $USERID -ne 0 ]; then
+    echo "Error:: please run this script with root privelege"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ # funsction receive the inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "Installing $2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "Installing $2 ... $G is success $N" 
+    fi   
+}
+
+dnf list installed mysql
+#Install if it is not found
+if [ $? -ne 0 ]; then
+    dnf install mysql -y
+    VALIDATE $? "MySQL"
+else
+    echo "MySQL Already exist ... $Y SKIPPING $N"
+fi
+dnf list installed nginx
+#Install if it is not found
+if [ $? -ne 0 ]; then
+    dnf install nginx -y
+    VALIDATE $? "ngnix"
+else
+    echo -e "Nginx Already exist ... $Y SKIPPING $N"
+fi
+dnf list installed python3
+#Install if it is not found
+if [ $? -ne 0 ]; then
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo -e "Nginx Already exist ... $Y SKIPPING $N"
+fi
