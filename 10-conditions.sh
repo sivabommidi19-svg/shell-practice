2945c599bc5098e077e2601869c325729861d7fa
sivabommidi19-svg <sivabommidi19@gmail.com>
2025-09-30 21:36:52 +0530

    session-06

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -5,7 +5,7 @@ NUMBER=$1
 if [$NUMBER -lt 10]; then
     echo "Given number $NUMBER is Less then 10"
 else
-    echo "Given number $NUMBER is granter then or equal to 10"
+    echo "Given number $NUMBER is greater then or equal to 10"
 fi 
  
  # -gt
