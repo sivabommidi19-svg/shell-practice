c04c7a61e3e21e3765107bb3f6bf44a3e83221c2
sivabommidi19-svg <sivabommidi19@gmail.com>
2025-10-06 19:20:35 +0530

    session-18

--- 18-Loops.sh
+++ 18-Loops.sh
@@ -34,7 +34,7 @@ do
   dnf list installed $package &>>$LOG_FILE
 
   #if exit status is =0 , already installed -ne 0 need to installed it
-    if [$? -ne 0]; then
+    if [ $? -ne 0 ]; then
         dnf install $package -y &>>$LOG_FILE
         VALIDATE $? "$Package"
     else
