0f1125bb0d86a8546f5c0add52d1069831284920
sivabommidi19-svg <sivabommidi19@gmail.com>
2025-10-08 23:13:05 +0530

    session-18

--- 19-set.sh
+++ 19-set.sh
@@ -2,7 +2,13 @@
 
 set -e
 
+error(){
+    echo "There is an error"
+}
+
+trap error ERR 
+
 echo "Hello"
 echo "Before error.."
-asvdjhavdfv; dnf
+asvdjhavdfv; dnf #here shell understand there is an error and signak is ERR
 echo "After error"
